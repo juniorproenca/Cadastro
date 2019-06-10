bfc7a847c37f045d66797bce6b4b0a44ea99ad7c
juniorproenca <40328458+juniorproenca@users.noreply.github.com>
2019-06-10 16:21:13 -0300

    Add files via upload

--- hello/polls/models.py
+++ hello/polls/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    SEXO_CHOICES = [
+        ["F", "Feminino"],
+        ["M", "Masculino"],
+        ["N", "Nenhuma das opções"]
+    ]
+    grau_de_instrucao = [
+        ["EM", "Ensino Medio"],
+        ["ES", "Ensino Superior"],
+        ["N", "Nenhuma das opções"]
+    ]
+
+    naturalidade2 = [
+        ["EM", "Brasil"],
+        ["ES", "Espanha"],
+        ["N", "Nenhuma das opções"]
+    ]
+
+
+
+
+    nome = models.CharField(max_length=20, null=False)
+    email = models.EmailField(null=False)
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    grau = models.CharField(max_length=1, choices=grau_de_instrucao)
+    naturalidade = models.CharField(max_length=1, choices=naturalidade2)
+
+
+
+ 
--- hello/polls/views.py
+++ hello/polls/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime 
+from .forms import *
 # Create your views here.
 
 def home (request):	
 	return render(request,'polls/home.html')
 
 def inscrição(request):	
-	return render(request,'polls/inscrição.html')
+	form = UsuarioForm()
+	return render(request,"polls/inscrição.html", {'form':form})
+
+
+def cadastrar_usuario(request):
+    form = UsuarioForm()
+    return render(request, 'form.html', {'form':form})
